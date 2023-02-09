05f67e78d3dfdd2f40b2c552a79662d794c98c59
SeoJin <84702722+DrunkJin@users.noreply.github.com>
2023-02-09 10:37:58 +0900

    p.40 c++구문

---
+++ C++_Primer_Plus/Ch02_C++시작하기/carrots.cpp
@@ -0,0 +1,20 @@
+// carrots.cpp -- 음식물 처리 프로그램
+// 하나의 변수를 사용하고 출력한다.
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    int carrots;            // 정수 변수를 선언한다.
+
+    carrots = 25;           // 변수에 값을 대입한다.
+    cout << "I have ";
+    cout << carrots;        // 변수의 값을 출력한다.
+    cout << "carrots";
+    cout << endl;
+    carrots << carrots - 1; // 변수의 값을 변경한다.
+    cout << "Crunch, Crunch. Now there are " << carrots << "carrots." << endl;
+    return 0;
+}
