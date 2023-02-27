8a04faa9465240fe5d36d577cc73bde8e03ccf9a
SeoJin <84702722+DrunkJin@users.noreply.github.com>
2023-02-27 15:15:02 +0900

    잠시 다시풀기

---
+++ C++_Primer_Plus/Ch03_데이터처리/Exercise/ch03.2.cpp
@@ -0,0 +1,33 @@
+// ch03.2.cpp -- 키를 피트와 인치 단위로 묻고 체중을 파운드 단위로 묻는 프로그램작성
+// 세개의 변수로 저장
+// BMI를 계산하여 보고해야함. 여러 환산인수를 위해 기호 상수를 사용할 것
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+    const int feet_to_inch = 12;
+    const double inch_to_meter = 0.0254, pound_to_kg = 2.2;
+    
+    int height_feet, height_inch;
+    double weight;
+    cout << "당신의 키에서 피트 부분만 입력해주세요. " ;
+    cin >> height_feet;
+    cout << "당신의 키에서 인치 부분만 입력해주세요. ";
+    cin >> height_inch;
+    cout << "당신의 몸무게(파운드)를 입력해주세요. ";
+    cin >> weight;
+    
+    double height_meter = (height_feet * feet_to_inch + height_inch) * inch_to_meter;
+    double weight_square_kg = (weight/pound_to_kg) * (weight/pound_to_kg);
+    cout << "당신의 BMI는 ";
+    cout << height_meter/weight_square_kg;
+    cout << "입니다." << endl;
+
+    return 0;
+}
+
+
+// 다시풀어보기
